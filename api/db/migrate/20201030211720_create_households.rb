acec6ebce302557625c897ff3bd9a1acc146ceba
石塚昌也 <ishizuka@MacBook-Pro.local>
2020-11-03 18:54:51 +0900

    Migrate: household table has spent_date, aamount and memo column.

--- api/db/migrate/20201030211720_create_households.rb
+++ api/db/migrate/20201030211720_create_households.rb
@@ -1,7 +1,8 @@
 class CreateHouseholds < ActiveRecord::Migration[6.0]
   def change
     create_table :households do |t|
-      t.date :spent_date
+      t.date :spent_date, null: false
+      t.integer :amount, null: false
       t.text :memo
 
       t.timestamps
