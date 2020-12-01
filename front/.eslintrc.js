7749838074b70c1a62aca2b1177eae36a2b5510a
石塚昌也 <ishizuka@MacBook-Pro.local>
2020-12-02 05:15:37 +0900

    Add: add eslint and prettier package by yarn, command is 'yarn add -D babel-eslint eslint eslint-config-prettier eslint-loader eslint-plugin-vue eslint-plugin-prettier prettier'

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -4,16 +4,23 @@ module.exports = {
     browser: true,
     node: true
   },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
   extends: [
-    'plugin:nuxt/recommended',
-    '@nuxtjs'
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
   ],
   plugins: [
+    'vue'
   ],
   rules: {
     'no-extra-semi': 'warn',
     semi: [2, 'never'],
     'no-console': 'off',
-    'vue/max-attributes-per-line': 'off'
+    'vue/max-attributes-per-line': 'off',
+    "prettier/prettier": ["error", { semi: false }],
+    "vue/html-self-closing": "off", //空タグを許可する
   }
 }
